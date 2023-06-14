a79cb3b7e2bad6b42d66a683b002ac8d91824955
John Mathew Pua <john.vstecs@gmail.com>
2023-06-14 14:23:31 +0800

    This is LE3

---
+++ PuaBlogDB/Stored Procedures/spUsers_Register.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[spUsers_Register]
+	@username nvarchar(16),
+	@firstname nvarchar(50),
+	@lastname nvarchar(50),
+	@password nvarchar(16)
+AS
+BEGIN
+	set nocount on;
+
+	INSERT INTO dbo.Users
+	(UserName, FirstName, LastName, Password)
+	VALUES(@username, @firstname, @lastname, @password)
+END
